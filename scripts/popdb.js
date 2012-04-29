05abf0d90e7ce18330a20bb4927a484eba404290
Zahan Malkani <zahanm@gmail.com>
2012-04-29 12:52:31 -0700

    no need to popdb with people

--- scripts/popdb.js
+++ scripts/popdb.js
@@ -13,52 +13,39 @@ Person = Mongoose.model('Person');
 Vote = Mongoose.model('Vote');
 
 // Make two people
-var p1, p2, b1, b2, r1, r2;
-p1 = new Person({
-  name: 'Brian Tolkin',
-  email: 'brian@babelon.co'
-});
-p1.save(function(err, d) {
+var b1, b2, r1, r2;
+
+// Make two businesses
+b1 = new Entity({
+  name: 'Mantra',
+  website: 'http://www.mantrapa.com/'
+})
+b1.save(function(err, d) {
   if (err) { console.error(err); }
-  p2 = new Person({
-    name: 'Aaron Kalb',
-    email: 'aaron@babelon.co'
-  });
-  p2.save(function(err, d) {
+  b2 = new Entity({
+    name: 'Sushi Tomo',
+    website: 'http://www.yelp.com/biz/sushi-tomo-palo-alto-2/'
+  })
+  b2.save(function(err, d) {
     if (err) { console.error(err); }
-    // Make two businesses
-    b1 = new Entity({
-      name: 'Mantra',
-      website: 'http://www.mantrapa.com/'
-    })
-    b1.save(function(err, d) {
+    // And two reviews
+    r1 = new Fragment({
+      entity: b1,
+      text: 'Casual Indian food restaurant that is frankly just not that good. The food was just meh, the service was only OK, and the menu seemed limiting. I know other people have had good experiences here, but I would definitely go to other Indian restaurants in Palo Alto over this one.',
+      author: p1,
+      truth: true
+    });
+    r1.save(function(err, d) {
       if (err) { console.error(err); }
-      b2 = new Entity({
-        name: 'Sushi Tomo',
-        website: 'http://www.yelp.com/biz/sushi-tomo-palo-alto-2/'
-      })
-      b2.save(function(err, d) {
-        if (err) { console.error(err); }
-        // And two reviews
-        r1 = new Fragment({
-          entity: b1,
-          text: 'Casual Indian food restaurant that is frankly just not that good. The food was just meh, the service was only OK, and the menu seemed limiting. I know other people have had good experiences here, but I would definitely go to other Indian restaurants in Palo Alto over this one.',
-          author: p1,
-          truth: true
-        });
-        r1.save(function(err, d) {
-          if (err) { console.error(err); }
-          r2 = new Fragment({
-            entity: b2,
-            text: 'Rolls here are pretty good. I went here on a date and the atmosphere wasn’t really what I expect. Normally for sushi I anticipate a smaller, Asian-style restaurant, but this seemed more American. Sushi itself was pretty good. Fish seemed pretty fresh and had some cool, unique rolls. Overall, I would for sure come back with a bunch of friends, but definitely not for a date. Also, would like to try things besides the sushi because they seemed pretty interesting. For sushi, had a very good price point. Point of note: make sure to understand what is in the crab rolls before ordering because some are imitation crab and some are real crab.',
-            author: p2,
-            truth: false
-          });
-          r2.save(function(err, d) {
-            console.log('all done loading into db');
-            process.exit(0);
-          });
-        });
+      r2 = new Fragment({
+        entity: b2,
+        text: 'Rolls here are pretty good. I went here on a date and the atmosphere wasn’t really what I expect. Normally for sushi I anticipate a smaller, Asian-style restaurant, but this seemed more American. Sushi itself was pretty good. Fish seemed pretty fresh and had some cool, unique rolls. Overall, I would for sure come back with a bunch of friends, but definitely not for a date. Also, would like to try things besides the sushi because they seemed pretty interesting. For sushi, had a very good price point. Point of note: make sure to understand what is in the crab rolls before ordering because some are imitation crab and some are real crab.',
+        author: p2,
+        truth: false
+      });
+      r2.save(function(err, d) {
+        console.log('all done loading into db');
+        process.exit(0);
       });
     });
   });
