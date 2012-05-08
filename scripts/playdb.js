1f15b30eb59c5663f255ce8498f05c156a9e90f2
Zahan Malkani <zahanm@gmail.com>
2012-05-08 02:19:53 -0700

    script to play with db data

---
+++ scripts/playdb.js
@@ -0,0 +1,15 @@
+var path, async, Mongoose, Fragment, Entity, Person, Vote;
+
+path = require('path');
+
+_ = require('underscore');
+async = require('async');
+Mongoose = require('mongoose');
+
+Mongoose.connect('mongodb://localhost/lietome');
+require( path.resolve( __dirname, '../lib/models') ).define(null);
+
+Fragment = Mongoose.model('Fragment');
+Entity = Mongoose.model('Entity');
+Person = Mongoose.model('Person');
+Vote = Mongoose.model('Vote');
