82685bdeca27efff10a82e6240987e716c2a0e73
Zahan Malkani <zahanm@gmail.com>
2012-05-08 03:46:35 -0700

    Mongoose already has a toObject() method

--- scripts/playdb.js
+++ scripts/playdb.js
@@ -13,3 +13,20 @@ Fragment = Mongoose.model('Fragment');
 Entity = Mongoose.model('Entity');
 Person = Mongoose.model('Person');
 Vote = Mongoose.model('Vote');
+
+tests = {
+  bag: function() {
+    Person.findOne({})
+    .run(function(err, p) {
+      console.log(p.toObject({ virtuals: true }));
+      process.exit(0);
+    });
+  }
+};
+
+if (process.argv.length === 3 && process.argv[2] in tests) {
+  tests[process.argv[2]]();
+} else {
+  console.log('usage:', process.argv[0], path.basename(process.argv[1]), '<operation>');
+  process.exit(0);
+}
