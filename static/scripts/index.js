1fdea6b1f66ad4acb3b114e97966d69b63ad32d7
Zahan Malkani <zahanm@gmail.com>
2012-05-09 17:25:48 -0700

    scripts in index.js, fragment length validation

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -2,9 +2,75 @@
 // Utilities
 (function(exports) {
 
-  function count_words (text) {
+  function countWords (text) {
     return text.split(/\s+/).filter(function(w) { return !!w; }).length;
   }
-  exports.count_words = count_words;
+  exports.countWords = countWords;
+
+  function triggerEvent (evtype, target) {
+    var ev = document.createEvent('HTMLEvents');
+    ev.initEvent(evtype, true, false);
+    target.dispatchEvent(ev);
+  }
+  exports.triggerEvent = triggerEvent;
 
 })(window.Utilities = {});
+
+// Handlers
+(function() {
+
+  // yes / no || authentic / deceptive buttons
+  var buttons = document.querySelectorAll('.form-inline > button');
+  Array.prototype.forEach.call(buttons, function(button) {
+    button.addEventListener('click', function(ev) {
+      var truth, choice;
+      truth = document.getElementById('input-truth');
+      choice = ev.target.textContent || ev.target.innerText;
+      if (choice.match(/yes/i) || choice.match(/authentic/i)) {
+        truth.value = 'true';
+      } else {
+        truth.value = 'false';
+      }
+    });
+  });
+
+  // stars
+  var starbuttons = document.querySelectorAll('.rating input[type=radio]:enabled');
+  var staricons = document.querySelectorAll('.star-rating-group i');
+  Array.prototype.forEach.call(starbuttons, function(starbutton) {
+    starbutton.addEventListener('click', function(ev) {
+      var rating, i;
+      rating = Number(ev.target.value);
+      for (i = 0; i < rating; i++) { staricons[i].className = "icon-star"; }
+      for (i = rating; i < staricons.length; i++) { staricons[i].className = "icon-star-empty"; }
+    });
+  });
+
+  // fragment word count
+  var fragment, wc, submit;
+  fragment = document.querySelector('textarea[name=fragment]');
+  wc = document.querySelector('#word-count');
+  submit = fragment.form.querySelector('button[type=submit]');
+  var minwords = 15;
+  if (fragment && wc) {
+    fragment.addEventListener('keyup', function(ev) {
+      var count = Utilities.countWords(fragment.value);
+      if (wc.textContent) { wc.textContent = String(count); }
+      else { wc.innerText = wc.innerText && String(count); }
+      if (count >= minwords) {
+        fragment.parentElement.className = "control-group success";
+        submit.disabled = '';
+      }
+      else {
+        fragment.parentElement.className = "control-group error";
+        submit.disabled = 'disabled';
+      }
+    });
+    Utilities.triggerEvent('keyup', fragment);
+  } else if (fragment || wc) {
+    console.warn('Either fragment or word-count box is uninitialized.');
+    console.warn(fragment);
+    console.warn(wc);
+  }
+
+}).call(window);
