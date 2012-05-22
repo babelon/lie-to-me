44ee0d492dad432052f7aeef187f85a9ac6ac598
Zahan Malkani <zahanm@gmail.com>
2012-05-22 04:53:01 -0700

    fill up profiles with basic info

--- scripts/playdb.js
+++ scripts/playdb.js
@@ -1,4 +1,4 @@
-var path, async, Mongoose, Fragment, Entity, Person, Vote;
+var path, async, Mongoose, Fragment, Entity, Person, Vote, Facebook;
 
 path = require('path');
 
@@ -14,12 +14,47 @@ Entity = Mongoose.model('Entity');
 Person = Mongoose.model('Person');
 Vote = Mongoose.model('Vote');
 
+Facebook = require( path.resolve( __dirname, '../lib/facebook') );
+
+function quit () {
+  Mongoose.disconnect();
+  process.exit(0);
+}
+
 tests = {
   bag: function() {
     Person.findOne({})
     .run(function(err, p) {
       console.log(p.toObject({ virtuals: true }));
-      process.exit(0);
+      quit();
+    });
+  },
+  fillup: function() {
+    Person.find({})
+    .run(function(err, people) {
+      async.forEachSeries(people, function(person, next) {
+        Facebook.get_profile_info (
+        person.fb_id,
+        ['id', 'first_name', 'last_name', 'gender', 'link'],
+        '',
+        function(err, info) {
+          if (err) { console.error(err); next(); return; }
+          person.first_name = info['first_name'];
+          person.last_name = info['last_name'];
+          person.gender = info['gender'];
+          person.profile_page = info['link'];
+          person.fb_id = info['id'];
+          person.save(function(err) {
+            if (err) { console.error(err); next(); return; }
+            console.log('Saved', person.name);
+            next();
+          });
+        });
+      }, function(err) {
+        if (err) { console.error(err); quit(); return; }
+        console.log('All done');
+        quit();
+      });
     });
   }
 };
